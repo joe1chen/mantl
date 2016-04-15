2d16e2d730590c0ff7f4cce14565c38eb0310197
Joseph Chen <joseph.chen@gmail.com>
2016-04-15 15:38:18 -0700

    Change zone for aws testing. Change testing to only do aws.

--- testing/aws.tf
+++ testing/aws.tf
@@ -1,14 +1,29 @@
+variable "amis" {
+  default = {
+    us-east-1      = "ami-6d1c2007"
+    us-west-2      = "ami-d2c924b2"
+    us-west-1      = "ami-af4333cf"
+    eu-central-1   = "ami-9bf712f4"
+    eu-west-1      = "ami-7abd0209"
+    ap-southeast-1 = "ami-f068a193"
+    ap-southeast-2 = "ami-fedafc9d"
+    ap-northeast-1 = "ami-eec1c380"
+    sa-east-1      = "ami-26b93b4a"
+  }
+}
+
 variable "build_number" {}
+variable "region" {default = "us-east-1"}
 
 provider "aws" {
-  region = "us-west-1"
+  region = "${var.region}"
 }
 
 module "aws-mantl-testing" {
   source = "./terraform/aws"
-  availability_zone = "us-west-1b"
+  availability_zone = "${var.region}a"
   ssh_username = "centos"
-  source_ami = "ami-af4333cf"
+  source_ami = "${lookup(var.amis, var.region)}"
   short_name = "mantl-ci-${var.build_number}"
   long_name = "ciscocloud-mantl-ci-${var.build_number}"
 
